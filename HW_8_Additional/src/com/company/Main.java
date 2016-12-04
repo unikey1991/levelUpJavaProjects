f3645928e2d8db47bca4daa1571ee6998b5d28d4
unikey1991 <unikey1991@gmail.com>
2016-12-04 23:09:06 +0200

    update HW_8 additional 04.12 23:08

--- HW_8_Additional/src/com/company/Main.java
+++ HW_8_Additional/src/com/company/Main.java
@@ -1,56 +1,62 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     static final String [] NUM_FROM_10_TO_19 = new String []{"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
     static final String [] NUM_FOR_TENS = new String[]{"twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
     static final String [] NUM_FOR_ONES = new String []{"","one","two","three","four","five","six","seven","eight","nine"};
-    static final String [] NUM_FOR_HUNDREDS = new String[]{"","one hundred","two hundred","three hundred","four hundred",
-            "five hundred","six hundred","seven hundred","eight hundred","nine hundred"};
 
     public static void main(String[] args) {
-        // write your code here
 
         int arr1[] = new int[]{-200, -54, 100, 0, 500, 55, 9999};
         int num4 = 6;
         int num5 = 22115;
         int num6[] = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        String str7 = "one hundred forty six";
+        String str7 = "seven hundred twenty one";
 
         System.out.println(task1(10, -4));
         System.out.println("Cумма только положительных из трех чисел = " + task2(arr1));
         System.out.println("Факториал числа " + num4 + "! = " + task4(num4));
         System.out.println("Сумма цифр числа " + num5 + " = " + task5(num5));
         System.out.println("Cумма элементов массива с нечетными индексами = " + task6(num6));
-        System.out.println(task7("one hundred"));
+        System.out.println(task7(str7));
        // System.out.println(str7.charAt(4) == ' ');
 
     }
 
     //7.Вводим строку, которая содержит число, написанное прописью (0-999). Получить само число. Например, дана строка: seven hundred fifty two = 752.
-    static String task7(String num){
-        int i = 0, j = 0, z=0;
+    static int task7(String num){
+        int count=0;
         String arr[] = new String[4];
-        String num1 = "";
-        while (i < num.length()){  // разбитие строки
+        String strNum1 = "", strNum2 = "";
+        int intNum = 0;
+        for (int i = 0, j = 0; i < num.length(); i++){  // разбитие строки на отдельные слова и занесение в массив
             if (num.charAt(i) == ' ') {
-                arr[z] = num.substring(j,i);
-            }
-            if (num.charAt(i) == ' ') {
+                arr[count] = num.substring(j,i);
+                count++;
                 j = i + 1;
-                z++;
             }
-            if (i == num.length()-1) arr[z] = num.substring(j,num.length());
-            i++;
+            if (i == num.length()-1) arr[count] = num.substring(j,num.length()); // если цикл достик последнего элемента, то строку от последнего элемента до индекса последнего пробела пишем в массив
         }
-        if (z>2){
-            arr[0] = arr[0]+" "+arr[1];
-            arr[1] = "";
+        for (int y = 0; y<count+1; y++){ // перебираем массив слов и сравниваем с константой
+            if (arr[y].equals("hundred")) strNum1 += "00";
+                for (int k = 0; k < NUM_FOR_ONES.length; k++){  // присваиваем переменным значения в зависимости от найденых
+                    if (arr[y].equals(NUM_FOR_ONES[k]) && y == 0)strNum1 += k;
+                    else if (arr[y].equals(NUM_FOR_ONES[k])) intNum += k;
+                }
+                for (int k = 0; k < NUM_FOR_TENS.length; k++){
+                    if (arr[y].equals(NUM_FOR_TENS[k]))strNum2 += (k+2)*10;
+                }
+                for (int k = 0; k < NUM_FROM_10_TO_19.length; k++){
+                    if (arr[y].equals(NUM_FROM_10_TO_19[k]) && y == 0) strNum1 += k+10;
+                    else if(arr[y].equals(NUM_FROM_10_TO_19[k])) strNum2 += k+10;
+                }
         }
-        for (int y = 0; y<4; y++){
-            if (findEquals(arr[y]) > 0) num1 += findEquals(arr[y]);
-        }
-        return num1;
+        if (strNum1.length()>0)intNum += Integer.parseInt(strNum1); // проверяем на не пустые строки
+        if (strNum2.length()>0)intNum += Integer.parseInt(strNum2);
+        return intNum;
     }
 
     static int findEquals(String x){
@@ -58,13 +64,10 @@ public class Main {
             if (x.equals(NUM_FOR_ONES[i]))return i;
         }
         for (int i = 0; i < NUM_FOR_TENS.length; i++){
-            if (x.equals(NUM_FOR_TENS[i]))return i;
+            if (x.equals(NUM_FOR_TENS[i]))return (i+2)*10;
         }
         for (int i = 0; i < NUM_FROM_10_TO_19.length; i++){
             if (x.equals(NUM_FROM_10_TO_19[i])) return i;
-        }
-        for (int i = 0; i < NUM_FOR_HUNDREDS.length; i++){
-            if (x.equals(NUM_FOR_HUNDREDS[i])) return i;
         }
         return 0;
     }
