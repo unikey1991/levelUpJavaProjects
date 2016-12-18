08602e770bbb816afcbdcb353243c9d3c72014fb
unikey1991 <unikey1991@gmail.com>
2016-12-19 00:54:15 +0200

    HW_13

---
+++ HW_13/src/com/company/BarTest.groovy
@@ -0,0 +1,46 @@
+package com.company
+
+/**
+ * Created by unike on 19.12.2016.
+ */
+class BarTest extends spock.lang.Specification {
+    def "ShareTip"() {
+
+    }
+
+    def "TakeTips"() {
+
+    }
+
+    def "AddPersonal"() {
+
+    }
+
+    def "DelPersonal"() {
+
+    }
+
+    def "PerformeOrder"() {
+
+    }
+
+    def "FillWarehouse"() {
+
+    }
+
+    def "RemoveFromArrayStarting"() {
+
+    }
+
+    def "RemoveOrder"() {
+
+    }
+
+    def "DecreaseAlcoholAmount"() {
+
+    }
+
+    def "AddOrder"() {
+
+    }
+}
