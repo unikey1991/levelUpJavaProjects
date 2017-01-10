8fed790825b69d99ba9ae1651724a179630bb908
unikey1991 <aleksej.mikhalchuk@gmail.com>
2017-01-10 21:00:46 +0200

    hhh

---
+++ Address_book/src/view/Dialog.java
@@ -0,0 +1,66 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by java on 10.01.2017.
+ */
+public abstract class Dialog extends JDialog {
+
+    protected JButton ok = null;
+    protected JButton cancel = null;
+    protected JPanel panel = new JPanel();
+    private final ActionListener listener;
+    private boolean isOkPressed = false;
+
+    public Dialog() {
+        setModal(true);
+        setLayout(null);
+        setBounds(300,400,250,240);
+        setResizable(false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        add(panel);
+        panel.setBounds(0,0,250,240);
+        panel.setLayout(null);
+
+        this.listener = getButtonListener();
+
+        this.ok = createActionButton("ok", new Rectangle(30,160,80,30));
+        this.cancel = createActionButton("cancel", new Rectangle(130,160,80,30));
+
+        panel.add(ok);
+        panel.add(cancel);
+    }
+
+    public boolean isOkPressed() {
+        boolean res = isOkPressed;
+        isOkPressed= false;
+        return res;
+    }
+
+    public void setOkPressed(boolean okPressed) {
+        isOkPressed = okPressed;
+    }
+
+    public abstract Object getEntity();
+
+    private JButton createActionButton(String name, Rectangle rectangle) {
+        JButton btn = new JButton(name);
+        btn.addActionListener(listener);
+        btn.setBounds(rectangle);
+        return btn;
+    }
+
+    private ActionListener getButtonListener() {
+        return e -> {
+            if (e.getActionCommand().equals("ok")){
+                isOkPressed = true;
+            }
+            dispose();
+        };
+    }
+}
---
+++ Address_book/src/view/impl/CitizenTablePanel.java
@@ -0,0 +1,57 @@
+package view.impl;
+
+import entity.Citizen;
+import view.Action;
+import view.CitizenTableModelContainer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+/**
+ * Created by java on 10.01.2017.
+ */
+public class CitizenTablePanel extends JPanel implements Action{
+
+    private final JTable table;
+    private final CreateCitizenDialog dialog = new CreateCitizenDialog();
+    private final CitizenTableModelContainer tableContainer;
+
+    public CitizenTablePanel() {
+        this.tableContainer = new CitizenTableModelContainer();
+        this.table = new JTable(tableContainer);
+        setName("Citizen Tab");
+        init();
+    }
+
+    private void init() {
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setSize(new Dimension(595,300));
+        add(scrollPane);
+        setSize(new Dimension(595,300));
+    }
+
+    @Override
+    public void create() {
+        dialog.setVisible(true);
+        if(dialog.isOkPressed()){
+            tableContainer.getData().add(dialog.getEntity());
+        }
+    }
+
+    @Override
+    public List<Citizen> read() {
+        return  tableContainer.getData();
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void delete() {
+        Citizen citizen = tableContainer.getSelectedRowData(table.getSelectedRow());
+        tableContainer.getData().remove(citizen);
+    }
+}
