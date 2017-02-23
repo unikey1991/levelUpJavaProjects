b1b7f4b75cc5aabcb616ae4f80a2e7bc00ea9f7b
unikey1991 <unikey1991@gmail.com>
2017-02-23 22:34:59 +0200

    HW_28 Работа с файлами и папками

--- lesson_28/src/explorer/CommandLineApp.java
+++ lesson_28/src/explorer/CommandLineApp.java
@@ -5,6 +5,10 @@ import java.io.*;
 import java.security.KeyStore;
 import java.util.Scanner;
 
+import static java.lang.System.in;
+import static java.lang.System.out;
+import static java.lang.System.setOut;
+
 /**
  * Created by java on 21.02.2017.
  */
@@ -13,34 +17,117 @@ public class CommandLineApp {
     private static final String DEFAULT_PATH = "D:\\";
     private static File currentDir = new File(DEFAULT_PATH);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(in);
+        System.out.println("Use Help to view commands");
 
         while (true) {
 
             System.out.println(currentDir.getPath());
             String line = scanner.nextLine();
 
-            if (line.startsWith("print")) {
+            if (line.split(" ")[0].equalsIgnoreCase("print")) {
                 if (currentDir.isDirectory()) {
                     printAllDirectories(currentDir);
                 }
-            } else if (line.startsWith("mkDir")) {  //mkDir newFolderName
+            } else if (line.split(" ")[0].equalsIgnoreCase("mkDir")) {  //mkDir newFolderName
                 createNewFolder(line, currentDir);
-            } else if (line.startsWith("cd")) {
+            } else if (line.split(" ")[0].equalsIgnoreCase("cd")) {
                 changeDirectory(line);
-            }
+            } else if (line.split(" ")[0].equalsIgnoreCase("mkFile")) {  //mkDir newFolderName
+                createNewFile(line, currentDir);
+            } else if (line.split(" ")[0].equalsIgnoreCase("delete")) {  //mkDir newFolderName
+                delete(line, currentDir);
+            } else if (line.split(" ")[0].equalsIgnoreCase("open")) {  //mkDir newFolderName
+                openTextFile(line, currentDir);
+            } else if (line.split(" ")[0].equalsIgnoreCase("edit")) {  //mkDir newFolderName
+                editTextFile(line, currentDir);
+            } else if (line.split(" ")[0].equalsIgnoreCase("help")) {  //mkDir newFolderName
+                    help();
+            } else System.out.println("Wrong command. Use Help to view commands");
         }
 
-}
+    }
+
+    private static void help() {
+        System.out.println("mkDir [file name]   создать папку");
+        System.out.println("cd [folder name]   сменить путь");
+        System.out.println("delete [folder name/file name]   удалить файл");
+        System.out.println("open [fileName.txt/cvs]   отобразить содержимое текстового файла");
+        System.out.println("edit [file name.txt/cvs]   редактировать файл");
+        System.out.println("mkFile [file name]   создать файл");
+        System.out.println("print   вывести список файлов и папок");
+    }
+
+    private static void editTextFile(String line, File parent) throws IOException {
+        String fileName = line.split("\\s+")[1];
+
+        if (!(fileName.endsWith(".txt") || fileName.endsWith(".cvs"))) {
+            System.out.println("Wrong file type to edit");
+            return;
+        }
+            File file = new File(parent, fileName);
+            if (!file.exists()){
+                System.out.println("File not exists");
+                return;
+            }
+            PrintWriter out = new PrintWriter(file);
+            Scanner scanner = new Scanner(System.in);
+            String s = "";
+            System.out.println("Write your text and use Save to save change and exit");
+            String text;
+            while (true) {
+                text = scanner.nextLine();
+                if (text.equals("save")) break;
+                s += (text+"\n");
+            }
+            out.print(s);
+            save(out, line, parent);
+    }
+
+    private static void save(PrintWriter text ,String line, File parent) throws IOException {
+        text.close();
+        System.out.println("File text edit to:");
+        openTextFile(line, parent);
+
+    }
+
+    private static void openTextFile(String line, File parent) throws IOException {
+        String fileName = line.split("\\s+")[1];
+
+        if (fileName.endsWith(".txt") || fileName.endsWith(".cvs")) {
+
+            File file = new File(parent, fileName);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String out;
+
+            while ((out = bufferedReader.readLine()) != null) {
+                System.out.println(out);
+            }
+        }
+    }
+
+    private static void delete(String line, File parent) {
+        String fileName = line.split("\\s+")[1];
+
+        File file = new File(parent, fileName);
+        if (!file.exists()){
+            System.out.println("File or directory not exists");
+            return;
+        }
+        if (file.isFile()) System.out.println("File "+fileName+" deleted");
+        if (file.isDirectory()) System.out.println("Directory "+fileName+" deleted");
+        file.delete();
+    }
+
 
     private static void changeDirectory(String line) {
         String folderName = line.split("\\s+")[1];
 
         if (folderName.contains("..")) {
-            if(currentDir.getParentFile() != null) currentDir = currentDir.getParentFile();
+            if (currentDir.getParentFile() != null) currentDir = currentDir.getParentFile();
             return;
         }
 
@@ -60,6 +147,14 @@ public class CommandLineApp {
 
     }
 
+    private static void createNewFile(String line, File parent) throws IOException {
+        String fileName = line.split("\\s+")[1];
+
+        File file = new File(parent, fileName);
+        file.createNewFile();
+        System.out.println("file "+fileName+ " created");
+    }
+
     private static void createNewFolder(String line, File parent) {
         String folderName = line.split("\\s+")[1];
 
@@ -70,7 +165,7 @@ public class CommandLineApp {
     private static void printAllDirectories(File file) {
         File[] files = file.listFiles();
         for (File f : files) {
-            if (f.isDirectory() && !f.isHidden()) {
+            if (!f.isHidden()) {
                 System.out.println(f.getName());
             }
         }
