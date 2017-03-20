e2331cac07d17386a4e7a9e31b08a68fb25f7f80
unikey1991 <unikey1991@gmail.com>
2017-03-20 22:56:49 +0200

    asf

--- jdbc-test1/src/main/java/Main.java
+++ jdbc-test1/src/main/java/Main.java
@@ -8,12 +8,13 @@ public class Main {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/address_book?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "password");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" +
+                    "address_book?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "qwerty");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM CITIZEN");
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getLong("ID") + " " + resultSet.getString("USERNAME") + " " + resultSet.getString("EMAIL"));
+                System.out.println(resultSet.getLong("ID") + " " + resultSet.getString("first_name") + " " + resultSet.getString("last_name"));
             }
 
             connection.close();
