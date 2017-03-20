525cd2f8496ef1a7c41a865d33edfde2565bb2af
unikey1991 <unikey1991@gmail.com>
2017-03-20 22:11:41 +0200

    asf

--- jdbc-test1/src/main/java/Main.java
+++ jdbc-test1/src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/address_book?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "fdnjvfabz");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/address_book?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "password");
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
 
