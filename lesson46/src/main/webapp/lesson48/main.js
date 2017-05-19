70de15fe6c95c5633e46564d1beea40bab5fe73c
valentindp <valentindp.post@gmail.com>
2017-05-19 20:56:52 +0300

    lesson47

---
+++ lesson46/src/main/webapp/lesson48/main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by java on 19.05.2017.
+ */
+
+console.log("   hello world");
+console.log('   hello world');
+
+function fireClick(){
+    console.log("fire!");
+
+    var login = document.getElementById("login");
+
+    login.value = "meeeent, valiii ot suda bla"
+}
+
+//var btn = document.getElementById("btn");
+
+//btn.addEventListener("click",fireClick);
